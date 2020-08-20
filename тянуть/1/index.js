a071768cbf60ff9a5ab88c69d117689ba3b1ba52
pavelmm <57666111+pavelmm@users.noreply.github.com>
2020-08-20 12:05:52 +0300

    Create index.js

---
+++ тянуть/1/index.js
@@ -0,0 +1,12 @@
+let firstRow = 'мама мыла раму'+' ';
+let secondRow = 'собака друг человека';
+ console.log(firstRow.length);
+ console.log(secondRow.length);
+
+
+for (let i = 0; i < firstRow.length ; ++i) {
+    console.log(firstRow.charAt(i));
+}
+for (let i = 0; i < secondRow.length; ++i) {
+    console.log(secondRow.charAt(i));
+}
