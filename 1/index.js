7c4d99d19c55f60783834496cfebd40df0582307
pavelmm <57666111+pavelmm@users.noreply.github.com>
2020-08-20 12:20:47 +0300

    Create index.js

---
+++ 1/index.js
@@ -0,0 +1,27 @@
+let firstRow = 'мама мыла раму'+' ';
+let secondRow = 'собака друг человека';
+ console.log(firstRow.length);
+ console.log(secondRow.length);
+
+
+for (let i = 0; i < firstRow.length ; ++i) {
+    console.log(firstRow.charAt(i));
+}
+for (let i = 0; i < secondRow.length; ++i) {
+    console.log(secondRow.charAt(i));
+}
+
+
+
+
+let s1 = " 7";
+    let s2 = "(123)";
+    let s3 = "456-78-90";
+    let firstRow = s1 +s2 + s3;
+      console.log("+" + s1 + " " + s2 + " " + s3);
+    
+    for (let i = 0; i < firstRow.length ; ++i) {
+
+console.log(firstRow.charAt(i));}
+
+
